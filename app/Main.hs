5497879142fa4fb4cd0db337f42f36b1a8bf11e2
Vitor <vitor@Vitors-Mac-mini.local>
2022-07-17 05:16:19 +0100

    .

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,34 @@
+{-# LANGUAGE DerivingVia #-}
 module Main where
 
 import MyLib (program)
+import Config
+import Control.Monad.IO.Class
+import Control.Monad.Reader
+import Console
+import Adapters.Console.IOConsole (IOConsoleT(IOConsoleT))
+import Filesystem (Filesystem)
+import Adapters.Filesystem.IOFilesystem (IOFileSystemT(IOFileSystemT))
+import ArticleStore (ArticleStore)
+import Adapters.ArticleStore.FsArticleStore (FsArticleStoreT(FsArticleStoreT))
+
+newtype App a = App { runApp :: Config -> IO a }
+  deriving (Functor, Applicative, Monad, MonadIO, MonadReader Config)
+    via ReaderT Config IO
+  deriving Console
+    via IOConsoleT App
+  deriving Filesystem
+    via IOFileSystemT App
+  deriving ArticleStore
+    via FsArticleStoreT App
+
+app :: App ()
+app = program
 
 main :: IO ()
 main = do
   print "hello world"
+  runApp app cfg
+  where
+    cfg :: Config
+    cfg = Config "articles/" ""
